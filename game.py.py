96c16facd49b7bc8807e4ef30917a905eaf405c5
Abolfazl Fazaeli <fazaeli.dev@gmail.com>
2026-07-12 19:20:41 +0330

    First version of Number Guess Game

---
+++ game.py.py
@@ -0,0 +1,18 @@
+import random
+secret = random.randint(1, 100)
+
+guess=int(input("Find the target number: "))
+
+tries = 0
+
+while guess!=secret:
+    tries = tries + 1
+    if guess > secret:
+        print("The target number is smaller")
+        guess=int(input("Try again: "))
+    elif guess < secret:
+        print("The target number is larger")
+        guess=int(input("Try again: "))
+tries = tries + 1
+print("Well done, your guess is correct.")
+print("Number of guesses: ",tries)
