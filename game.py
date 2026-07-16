825da29274feca3f7a0b16d4786f654979a0b08e
Abolfazl Fazaeli <afaztabriz@gmail.com>
2026-07-17 02:14:24 +0330

    Add play again

--- game.py
+++ game.py
@@ -1,18 +1,22 @@
 import random
-secret = random.randint(1, 100)
+play_again = "y"
+while play_again == "y":
+    secret = random.randint(1, 100)
 
-guess=int(input("Find the target number: "))
+    guess=int(input("Find the target number: "))
 
-tries = 0
+    tries = 0
 
-while guess!=secret:
+    while guess!=secret:
+        tries = tries + 1
+        if guess > secret:
+            print("The target number is smaller")
+            guess=int(input("Try again: "))
+        elif guess < secret:
+            print("The target number is larger")
+            guess=int(input("Try again: "))
+
     tries = tries + 1
-    if guess > secret:
-        print("The target number is smaller")
-        guess=int(input("Try again: "))
-    elif guess < secret:
-        print("The target number is larger")
-        guess=int(input("Try again: "))
-tries = tries + 1
-print("Well done, your guess is correct.")
-print("Number of guesses: ",tries)
+    print("Well done, your guess is correct.")
+    print("Number of guesses: ",tries)
+    play_again = input("Play again? (y/n): ").lower()
